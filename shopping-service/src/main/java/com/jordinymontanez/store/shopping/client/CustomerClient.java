b924e32b4c36aaa3aa63d15e1d9552b48e3ffb8b
Jordiny Montañez Flores <jordiny.montanez.fr@viaexperis.pe>
2020-12-28 21:25:37 -0500

    update shopping-service

--- shopping-service/src/main/java/com/jordinymontanez/store/shopping/client/CustomerClient.java
+++ shopping-service/src/main/java/com/jordinymontanez/store/shopping/client/CustomerClient.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-/*@FeignClient(name = "customer-service" , fallback = CustomerHystrixFallbackFactory.class)*/
-@FeignClient(name = "customer-service")
-@RequestMapping("/customers")
+@FeignClient(name = "customer-service" , fallback = CustomerHystrixFallbackFactory.class)
+//@FeignClient(name = "customer-service")
+//@RequestMapping("/customers")
 public interface CustomerClient {
 
     @GetMapping(value = "/{id}")
